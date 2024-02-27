63cad9f47a9a22194f609472047037d8e6654353
thilaga <thilagab@hotmail.com>
2024-02-27 21:05:36 +1100

    Initial Commit

---
+++ EmojiCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiCollectionView
+//
+//  Created by Thilagawathy Duraisamy on 27/2/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
